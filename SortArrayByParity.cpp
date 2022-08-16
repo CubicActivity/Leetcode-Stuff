7c4aa4b271470a1041ffd5c9d7e50fe312022a7a
The13ms <107684060+The13ms@users.noreply.github.com>
2022-08-16 16:37:10 +0200

    Add files via upload

---
+++ SortArrayByParity.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        int i = 0, j = nums.size() - 1;
+        while (i < j) {
+            if (nums[i] % 2 > nums[j] % 2) {
+                int tmp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = tmp;
+            }
+
+            if (nums[i] % 2 == 0) i++;
+            if (nums[j] % 2 == 1) j--;
+        }
+
+        return nums;
+    }
+};
