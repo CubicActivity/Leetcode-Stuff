ff85aecc7bad4980379570b29ed1903830fd6bf5
The13ms <107684060+The13ms@users.noreply.github.com>
2022-08-16 17:01:39 +0200

    Add files via upload

---
+++ HeightChecker.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        vector <int> expected=heights;
+        sort(expected.begin(),expected.end());
+        int unmatch=0;
+        for(int i=0;i<heights.size();i++){
+            if(expected[i]!=heights[i]){
+                unmatch++;
+            }
+        }return unmatch;
+    }
+};
