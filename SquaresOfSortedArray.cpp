72ee969f1cbfc7b52dd953c08aae959c990fc047
The13ms <107684060+The13ms@users.noreply.github.com>
2022-08-16 20:08:23 +0200

    Update SquaresOfSortedArray.cpp

--- SquaresOfSortedArray.cpp
+++ SquaresOfSortedArray.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        vector <int> squares(nums.size());
         for(int i=0;i<nums.size();i++){
-            squares[i]=nums[i]*nums[i];
-            
+            nums[i]*=nums[i];
         }
-        sort(squares.begin(),squares.end());
-        return squares;
+        sort(nums.begin(),nums.end());
+        return nums;
     }
 };
