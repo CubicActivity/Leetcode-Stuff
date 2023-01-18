38e3d77d545596fc36aa069c151e60c2ffc55c0a
The13ms <107684060+The13ms@users.noreply.github.com>
2023-01-18 20:06:54 +0100

    Add files via upload

---
+++ Two_Sum_hashmap.cpp
@@ -0,0 +1,13 @@
+#include <unordered_map>
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+       unordered_map <int,int> map;
+       for(int i=0; i<nums.size(); i++){
+           if(map.find(target-nums[i])==map.end())
+                map[nums[i]]=i;
+            else
+                return {map[target-nums[i]],i};
+       }return {-1, -1};
+    }
+};
